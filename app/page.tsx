430971af7a14e68d602a046f3eb3bb39195a9976
Ehsanullah Dehzad <e.dehzad1@gmail.com>
2025-09-07 23:10:29 -0400

    remove the card component from the charts

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,37 @@
-
+import { AppAreaChart } from "@/components/AppAreaChart";
+import AppBarChart from "@/components/AppBarChar";
+import { AppPieChart } from "@/components/AppPieChart";
+import CardList from "@/components/CardList";
 
 export default function Home() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-        Test
+        <h2 className="text-xl">Total Revenue</h2>
+        <p className="text-sm text-gray-400">
+          Showing total visitors for the last 6 months{" "}
+        </p>
+        <AppBarChart />
       </div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title="Popular Content" />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <h2 className="text-xl">Browser Usage</h2>
+        <p className="text-sm text-gray-400">January - June 2024 </p>
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground p-4 rounded-lg">
+        <CardList title="Latest Transactions" />
+      </div>
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-        Test
+        <h2 className="text-xl">Area Chart - Legend</h2>
+        <p className="text-sm text-gray-400">
+          Showing total visitors for the last 6 months
+        </p>
+        <AppAreaChart />
       </div>
       <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
     </div>
   );
-} 
+}
